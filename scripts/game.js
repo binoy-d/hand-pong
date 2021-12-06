a534d391728111bfccb56978f3614cc6fb3bd654
Daniel Binoy <dbinoy15@gmail.com>
2021-12-06 01:28:54 -0800

    add credit

--- scripts/game.js
+++ scripts/game.js
@@ -440,6 +440,11 @@ function tickBall() {
             if (ball.x > p.x - pw / 2 && ball.x < p.x + pw / 2) {
                 ball.vx = -ball.vx;
                 ball.x+=ball.vx;
+                if (ball.y > p.y - ph / 2 && ball.y < p.y + ph / 2){
+                    if (ball.x > p.x - pw / 2 && ball.x < p.x + pw / 2) {
+                        ball.x+=ball.vx;   
+                    }
+                }
             }
         }
     });
