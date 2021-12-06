d842c2c093211d4971b997cee16b42cb616ff4f2
Daniel Binoy <dbinoy15@gmail.com>
2021-12-06 13:36:48 -0800

    d

--- scripts/game.js
+++ scripts/game.js
@@ -11,7 +11,8 @@ const trackingButtonHTML = '<button onclick = "toggleVideo()" class="btn btn-pri
 
 const modelParams = {
     flipHorizontal: true, // flip e.g for video
-    maxNumBoxes: 20, // maximum number of boxes to detect
+    maxNumBoxes: 6, // maximum number of boxes to detect
+    imageScaleFactor: 0.5,
     iouThreshold: 0.5, // ioU threshold for non-max suppression
     scoreThreshold: 0.6, // confidence threshold for predictions.
 };
